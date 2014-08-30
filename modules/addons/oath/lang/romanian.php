d8ea86ea71b7523716d6b4dfdbc0f94343b30316
Daniel Neculai <daniel.neculai@gmail.com>
2014-08-30 13:12:58 +0000

    Romanian translation file (romanian.php) for WHMCS OATH Addon

---
+++ modules/addons/oath/lang/romanian.php
@@ -0,0 +1,28 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * *
+ * Romanian translation for WHMCS OATH Addon
+ * provided by LEX Media Concepts - www.lexmedia.ro
+ * * * * * * * * * * * * * * * * * * * * * */
+$_ADDONLANG['incorrect']='Codul introdus este incorect.';
+$_ADDONLANG['enterCodeNote']='Va rugam introduceti codul generat de aplicatia Dvs. mobila.';
+$_ADDONLANG['enterCode']='Introduceti codul';
+$_ADDONLANG['btnLogin']='Validare cod';
+$_ADDONLANG['enterEmCode']='Introduceti codul de urgenta';
+$_ADDONLANG['emLogin']='Autentificare de urgenta';
+$_ADDONLANG['lostDevice']='Ati pierdut dispozitivul mobil?';
+$_ADDONLANG['inactive']='Suportul pentru autentificare in doi pasi este momentan dezactivat.';
+$_ADDONLANG['disabled']='Autentificarea in doi pasi este momentan dezactivata.';
+$_ADDONLANG['btnEnable']='Activati autentificarea in doi pasi';
+$_ADDONLANG['scanNote']='Va rugam scanati acest cod QR cu aplicatia Dvs. mobila.';
+$_ADDONLANG['unableScan']='Daca nu il puteti scana, folositi aceast cod secret:';
+$_ADDONLANG['verify']='Verificare cod';
+$_ADDONLANG['recomApp']='Aplicatii recomandate';
+$_ADDONLANG['enabled']='Autentificarea in doi pasi este momentan activata.';
+$_ADDONLANG['emCode']='Codul de urgenta este';
+$_ADDONLANG['emCodeNote']='Veti putea folosi acest cod in cazul in care va veti pierdeti dispozitivul mobil.';
+$_ADDONLANG['btnSecret']='Vizualizare cod de urgenta';
+$_ADDONLANG['emCodeNote2']='Notati/salvati acest cod, deoarece nu va mai fi afisat din nou.';
+$_ADDONLANG['btnDisable']='Dezactivati autentificarea in doi pasi';
+$_ADDONLANG['note']='Nota: Orice sub-utilizator cu acces la contul Dvs. va trebui sa furnizeze un cod de autentificare in doi pasi.';
+
+?>
