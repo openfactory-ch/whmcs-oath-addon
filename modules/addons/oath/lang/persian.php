92f16c736ca10743023ec13cd2aefbbe352d6720
Dionysius <dragon.dionysius@gmail.com>
2016-09-18 17:32:19 +0200

    added languages: persian (thank you @aminmahdi), portuguese-br (thank you @menigma2)

---
+++ modules/addons/oath/lang/persian.php
@@ -0,0 +1,30 @@
+<?php
+//
+// Company: ARS NETWORK (M) SDN BHD
+// WebSite: www.ars-network.com
+// eMail: developer@ars-network.com
+// Phone: +60 (3) 8943-1261
+// 
+$_ADDONLANG['incorrect']='کد شما اشتباه است.';
+$_ADDONLANG['enterCodeNote']='لطفا کد تولید شده توسط نرم افزار موبایل را وارد نمایید.';
+$_ADDONLANG['enterCode']='کد را وارد نمایید';
+$_ADDONLANG['btnLogin']='ناییدیه ورود';
+$_ADDONLANG['enterEmCode']='لطفا کد اضطراری خود را وارد نمایید';
+$_ADDONLANG['emLogin']='ورود اضطراری';
+$_ADDONLANG['lostDevice']='موبایل خود را گم کرده اید؟';
+$_ADDONLANG['inactive']='اعتبار سنجی دو مرحله ای پشتیبانی در حال حاضر فعال نمی باشد.';
+$_ADDONLANG['disabled']='اعتبار سنجی دو مرحله ای در حال حاضر فعال نمی باشد.';
+$_ADDONLANG['btnEnable']='فعال سازی اعتبار سنجی دو مرحله ای ';
+$_ADDONLANG['scanNote']='لطفا این QR code را توسط نرم افزار authenticator موبایل اسکن نمایید.';
+$_ADDONLANG['unableScan']='اگر شما دسترسی به اسکن ندارید، از این کد استفاده نمایید:';
+$_ADDONLANG['verify']='تايید کد';
+$_ADDONLANG['recomApp']='توصیه نرم افزار';
+$_ADDONLANG['enabled']='اعتبار سنجی دو مرحله ای پشتیبانی در حال حاضر فعال می باشد.';
+$_ADDONLANG['emCode']='این کد اضطراری شما';
+$_ADDONLANG['emCodeNote']='شما می توانید از این کد استفاده نمایید در هر زمانی که موبایل خود را گم کردید.';
+$_ADDONLANG['btnSecret']='نمایش محرمانه';
+$_ADDONLANG['emCodeNote2']='متن زیر را یادداشت نمایید، به دلیل اینکه دیگر نمایش داده نخواهد شد.';
+$_ADDONLANG['btnDisable']='غیر فعال سازی اعتبار سنجی دو مرحله ای';
+$_ADDONLANG['note']='توجه: هر کاربری جهت دسترسی به حساب کاربری خود نیازمند نیاز به تایید اعتبار دومرحله ای دارد.';
+$_ADDONLANG['pagetitle'] = 'تایید ورود اعتبار دومرحله ای';
+?>
---
+++ modules/addons/oath/lang/portuguese-br.php
@@ -0,0 +1,25 @@
+<?php
+
+$_ADDONLANG['incorrect']='Seu código estava incorreta.';
+$_ADDONLANG['enterCodeNote']='Digite o código gerado pela sua aplicação móvel.';
+$_ADDONLANG['enterCode']='Insira seu código';
+$_ADDONLANG['btnLogin']='Validar Entrada';
+$_ADDONLANG['enterEmCode']='Digite o seu código de emergência';
+$_ADDONLANG['emLogin']='Acesso de Emergência';
+$_ADDONLANG['lostDevice']='Perdeu seu dispositivo móvel?';
+$_ADDONLANG['inactive']='O suporte à Autenticação de Dois Fatores está desativada.';
+$_ADDONLANG['disabled']='Autenticação de Dois Fatores está desativada.';
+$_ADDONLANG['btnEnable']='Habilitar a Autenticação de dois Fatores';
+$_ADDONLANG['scanNote']='Por favor ler este código QR com o seu aplicativo autenticador móvel.';
+$_ADDONLANG['unableScan']='Se não for possível fazer a varredura, use esta código:';
+$_ADDONLANG['verify']='Código de Verificação';
+$_ADDONLANG['recomApp']='App Recomendado';
+$_ADDONLANG['enabled']='Autenticação de Dois Fatores está ativado no momento.';
+$_ADDONLANG['emCode']='O seu código de emergência é';
+$_ADDONLANG['emCodeNote']='Você pode usar este se você perder o seu dispositivo móvel.';
+$_ADDONLANG['btnSecret']='Ver Código';
+$_ADDONLANG['emCodeNote2']='Anote isso, porque não será exibido novamente.';
+$_ADDONLANG['btnDisable']='Desativar Autenticação de Dois Fatores';
+$_ADDONLANG['note']='Nota: Quaisquer sub-usuários com acesso à sua conta será necessário fornecer um token de autenticação de dois fatores.';
+
+?>
