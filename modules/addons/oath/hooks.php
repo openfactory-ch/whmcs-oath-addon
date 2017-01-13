34cdddbe199fcda00e453ae89996edf6fade4356
Denis Manente <denis@manente.ch>
2017-01-13 21:41:35 +0100

    fix some issues with header always overwriting

--- modules/addons/oath/hooks.php
+++ modules/addons/oath/hooks.php
@@ -8,24 +8,24 @@ if(!defined("WHMCS")) {
  
 add_hook('ClientAreaPrimarySidebar', 1, function (MenuItem $menu)
 {
-    if (!is_null($menu->getChild('My Account'))) {
+	if (!is_null($menu->getChild('My Account'))) {
 		$menu->getChild('My Account')->addChild('OATH', array(
 			'label' => Lang::trans('twofactorauth'),
 			'uri' => 'index.php?m=oath',
 			'order' => '51',
 		));
-    }
+	}
 });
  
 add_hook('ClientAreaSecondaryNavbar', 1, function (MenuItem $menu)
 {
-    if (!is_null($menu->getChild('Account')) && !is_null(Menu::context('client'))) {
+	if (!is_null($menu->getChild('Account')) && !is_null(Menu::context('client'))) {
 		$menu->getChild('Account')->addChild('OATH', array(
 			'label' => Lang::trans('twofactorauth'),
 			'uri' => 'index.php?m=oath',
 			'order' => '51',
 		));
-    }
+	}
 });
 
 function oath_hook_client_login($vars) {
@@ -33,7 +33,7 @@ function oath_hook_client_login($vars) {
 		return;
 	}
 	
-    $userid = $vars['userid'];
+	$userid = $vars['userid'];
 	if(!get_query_val('mod_oath_client', 'secret', "userid = '{$vars['userid']}'")) {
 		if(isset($_SESSION['twofactorverify'])) {
 			unset($_SESSION['twofactorverify']);
@@ -58,23 +58,23 @@ function oath_hook_client_login($vars) {
 	# fix security vulnerabilty (issue #9): The automatic login will continue to login the user even when it got removed by our client login hook.
 	# we need to ensure there will be no cookies passed to browser with autologin information and thus the cookie WHMCSUser will be overwritten
 	# befor it gets send to the browser.
-    $headers_cookie_safe = array();
-    $all_headers=headers_list();
-    foreach($all_headers as $header) {
-            $yo = '/^Set-Cookie: vs '.$header;
-            if(preg_match('/^Set-Cookie: /', $header)) {
-                    if(!preg_match('/^Set-Cookie: WHMCSUser=/', $header)) {
-                            $headers_cookie_safe[] = $header;
-                    }
-            }
-    }
-    header_remove('Set-Cookie');
-    //for all safe headers print them out as they were
-    foreach($headers_cookie_safe as $header) {
-            header($header);
-    }
-    //invoke deletion of any existing autologin cookies
-    header('Set-Cookie: WHMCSUser=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT; Max-Age=0; path=/; httponly');
+	$headers_cookie_safe = array();
+	$all_headers=headers_list();
+	foreach($all_headers as $header) {
+			if(preg_match('/^Set-Cookie: /', $header)) {
+					if(!preg_match('/^Set-Cookie: WHMCSUser=/', $header)) {
+							$headers_cookie_safe[] = $header;
+					}
+			}
+	}
+	header_remove('Set-Cookie');
+	//for all safe headers print them out as they were
+	//actually multiple values here would overwrite each other the way header works... http://stackoverflow.com/q/34664208
+	foreach($headers_cookie_safe as $header) {
+			header($header);
+	}
+	//invoke deletion of any existing autologin cookies
+	setcookie('WHMCSUser', 'deleted', time()-3600, '/', "",false, true)
 
 	header('Location: index.php?m=oath');
 	exit(0);
@@ -89,7 +89,7 @@ function oath_hook_admin_logout($vars) {
 add_hook("AdminLogout", 0, "oath_hook_admin_logout");
 
 function oath_admin_page($vars) {
-    session_start();
+	session_start();
 	$script = explode('/', $_SERVER['SCRIPT_NAME']);
 	if(($script[count($script) - 1] == 'addonmodules.php' && $_GET['module'] == 'oath') || $_SESSION['twofactoradmin'] == $_SESSION['adminid']) {
 		return;
