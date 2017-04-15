d6bac3de7d945add5f360ca011cebcd041711fb1
Dionysius <dragon.dionysius@gmail.com>
2017-04-15 18:29:17 +0200

    add portuguese-pt language

---
+++ modules/addons/oath/lang/portuguese-pt.php
@@ -0,0 +1,25 @@
+<?php
+
+$_ADDONLANG['incorrect']='O seu código estava incorreto.';
+$_ADDONLANG['enterCodeNote']='Digite o código gerado pela sua aplicação móvel.';
+$_ADDONLANG['enterCode']='Insira o seu código';
+$_ADDONLANG['btnLogin']='Validar Autenticação';
+$_ADDONLANG['enterEmCode']='Digite o seu código de emergência';
+$_ADDONLANG['emLogin']='Acesso de Emergência';
+$_ADDONLANG['lostDevice']='Perdeu o seu dispositivo móvel?';
+$_ADDONLANG['inactive']='O suporte a Autenticação de 2 Factores está desactivado.';
+$_ADDONLANG['disabled']='A Autenticação de 2 Factores está descativada.';
+$_ADDONLANG['btnEnable']='Activar a Autenticação de 2 Factores';
+$_ADDONLANG['scanNote']='Por favor digitalize este código QR com a sua aplicação de autenticação móvel.';
+$_ADDONLANG['unableScan']='Se não for possível fazer scan do QR code, use este código:';
+$_ADDONLANG['verify']='Código de Verificação';
+$_ADDONLANG['recomApp']='App Recomendada';
+$_ADDONLANG['enabled']='Autenticação de 2 Factores está activa de momento.';
+$_ADDONLANG['emCode']='O seu código de emergência é';
+$_ADDONLANG['emCodeNote']='Poderá usar este código se perder o seu dispositivo móvel.';
+$_ADDONLANG['btnSecret']='Ver Código';
+$_ADDONLANG['emCodeNote2']='Anote esta informação. Não será exibida novamente.';
+$_ADDONLANG['btnDisable']='Desactivar Autenticação de 2 Factores';
+$_ADDONLANG['note']='Nota: Quaisquer sub-utilizadores com acesso à sua conta terão que fornecer um token de autenticação de 2 factores.';
+
+?>
